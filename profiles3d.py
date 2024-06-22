c5254e607eab818baf583ac7003f3f3dbebf0066
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-22 13:18:03 -0300

    sort tracers method added

--- profiles3d.py
+++ profiles3d.py
@@ -6,6 +6,11 @@ class Tracer:
         self.y = y
         self.z = z
         self.lm = lm
+        self.d = None # dist to a center (xc,yc,zc)
+
+    def distanceto(self, xc, yc , zc):
+        d2 = (self.x - xc)**2 + (self.y - yc)**2 + (self.z - zc)**2
+        self.d = np.sqrt(d2)
 
 
 class Void:
@@ -31,15 +36,19 @@ class Void:
         for i in range(len(cat)):
             self.tr.append(Tracer(cat.xhalo[i], cat.yhalo[i], cat.zhalo[i], cat.lmhalo[i]))
 
+    def sort_tracers(self):
+        self.tr.sort(key = lambda x: x.d)
 
     def radial_density_profile(self, rmin, rmax, dr=0.5):
 
         # dr en Mpc, distancia entre cascarones
-        mean_num_trac = 1.
+
+        # MeanNumTrac = 1 => Delta == Density
+        MeanNumTrac = 1.
 
         shells = [] # acá defino todas las cascaras que se van a dividir
         for shell in shells:
             
             mass = 1 # masa del cascaron
             volume = 1 # volumen del cascaron
-            Delta = (mass/volume)/mean_num_trac - 1.
+            Delta = (mass/volume)/MeanNumTrac - 1.
