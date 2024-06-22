6f8c1d963f68dbeb774d612fd2bfba339cd19d8f
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-22 04:13:23 -0300

    Adding profiles3d, for radial density profiles of voids

---
+++ profiles3d.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Tracer:
+    def __init__(self,x,y,z,lm):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.lm = lm
+
+
+class Void:
+    def __init__(self,xc,yc,zc,rv):
+        self.xc = xc
+        self.yc = yc
+        self.zc = zc
+        self.rv = rv
+        self.tr = [] #tracers
+
+    def comoving_to_celestial(self):
+        # delta = 
+        # alpha =
+        # redshift = astropy tiene una formula...
+        pass
+
+    def get_tracers(self, cat, RMAX=5.):
+        square_distance = (cat.xhalo - self.xc)**2 +  (cat.yhalo - self.yc)**2 + (cat.zhalo - self.zc)**2
+        mask = square_distance<=(RMAX*self.rv)**2
+
+        cat = cat[mask] # ver si esto no es el problema! (idem a perfiles3d.py de codes_tesis/)
+
+        for i in range(len(cat)):
+            self.tr.append(Tracer(cat.xhalo[i], cat.yhalo[i], cat.zhalo[i], cat.lmhalo[i]))
+
+
+    def radial_density_profile(self, rmin, rmax, dr=0.5):
+
+        # dr en Mpc, distancia entre cascarones
+        mean_num_trac = 1.
+
+        shells = [] # acá defino todas las cascaras que se van a dividir
+        for shell in shells:
+            
+            mass = 1 # masa del cascaron
+            volume = 1 # volumen del cascaron
+            Delta = (mass/volume)/mean_num_trac - 1.
---
+++ tests/generate_cat.py
@@ -0,0 +1,32 @@
+import numpy as np
+from astropy.io import fits
+
+#x,y,z,m
+lencat = 100
+l = 1000. #box size
+lm_min = 9.
+lm_max = 15.
+
+xhalo = np.random.uniform(-l,l,lencat)
+yhalo = np.random.uniform(-l,l,lencat)
+zhalo = np.random.uniform(-l,l,lencat)
+lmhalo = np.random.uniform(lm_min,lm_max,lencat)
+
+header = fits.Header()
+header.append( ('lencat', lencat) )
+header.append( ('L', l) )
+header.append( ('lm_min', lm_min) )
+header.append( ('lm_max', lm_max) )
+
+tabla = [
+    fits.Column(name='xhalo', format='E', array=xhalo),
+    fits.Column(name='yhalo', format='E', array=yhalo),
+    fits.Column(name='zhalo', format='E', array=zhalo),
+    fits.Column(name='lmhalo', format='E', array=lmhalo),
+         ]
+
+t = fits.BinTableHDU.from_columns(fits.ColDefs(tabla))
+hdu = fits.PrimaryHDU(header=header)
+hdul = fits.HDUList([hdu, t])
+
+hdul.writeto('testcat.fits', overwrite=True)
