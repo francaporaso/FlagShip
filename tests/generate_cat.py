503b6db58ffe9418df14aac2d95d053a47f28629
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-23 18:53:07 -0300

    ignoring __pycache__, *.fits and adding generate_cat.py

--- tests/generate_cat.py
+++ tests/generate_cat.py
@@ -1,32 +1,61 @@
 import numpy as np
 from astropy.io import fits
 
-#x,y,z,m
-lencat = 100_000
-l = 1000. #box size
-lm_min = 9.
-lm_max = 15.
 
-xhalo = np.random.uniform(-l,l,lencat)
-yhalo = np.random.uniform(-l,l,lencat)
-zhalo = np.random.uniform(-l,l,lencat)
-lmhalo = np.random.uniform(lm_min,lm_max,lencat)
+def uniform_cat(lencat=100_000, l=1000., lm_min=9., lm_max=15.):
 
-header = fits.Header()
-header.append( ('lencat', lencat) )
-header.append( ('L', l) )
-header.append( ('lm_min', lm_min) )
-header.append( ('lm_max', lm_max) )
+    xhalo = np.random.uniform(-l,l,lencat)
+    yhalo = np.random.uniform(-l,l,lencat)
+    zhalo = np.random.uniform(-l,l,lencat)
+    lmhalo = np.random.uniform(lm_min,lm_max,lencat)
 
-tabla = [
-    fits.Column(name='xhalo', format='E', array=xhalo),
-    fits.Column(name='yhalo', format='E', array=yhalo),
-    fits.Column(name='zhalo', format='E', array=zhalo),
-    fits.Column(name='lmhalo', format='E', array=lmhalo),
-         ]
+    header = fits.Header()
+    header.append( ('lencat', lencat) )
+    header.append( ('L', l) )
+    header.append( ('lm_min', lm_min) )
+    header.append( ('lm_max', lm_max) )
 
-t = fits.BinTableHDU.from_columns(fits.ColDefs(tabla))
-hdu = fits.PrimaryHDU(header=header)
-hdul = fits.HDUList([hdu, t])
+    tabla = [
+        fits.Column(name='xhalo', format='E', array=xhalo),
+        fits.Column(name='yhalo', format='E', array=yhalo),
+        fits.Column(name='zhalo', format='E', array=zhalo),
+        fits.Column(name='lmhalo', format='E', array=lmhalo),
+            ]
 
-hdul.writeto('tests/testcat.fits', overwrite=True)
+    t = fits.BinTableHDU.from_columns(fits.ColDefs(tabla))
+    hdu = fits.PrimaryHDU(header=header)
+    hdul = fits.HDUList([hdu, t])
+
+    hdul.writeto('tests/testcat_uniform.fits', overwrite=True)
+
+def normal_cat(lencat=100_000, sigma=0., mu=300, lm_min=9., lm_max=15.):
+
+    xhalo = np.random.normal(sigma,mu,lencat)
+    yhalo = np.random.normal(sigma,mu,lencat)
+    zhalo = np.random.normal(sigma,mu,lencat)
+    lmhalo = np.random.uniform(lm_min,lm_max,lencat)
+
+    l = np.array([xhalo, yhalo, zhalo]).flatten().max()
+
+    header = fits.Header()
+    header.append( ('lencat', lencat) )
+    header.append( ('L', l) )
+    header.append( ('lm_min', lm_min) )
+    header.append( ('lm_max', lm_max) )
+
+    tabla = [
+        fits.Column(name='xhalo', format='E', array=xhalo),
+        fits.Column(name='yhalo', format='E', array=yhalo),
+        fits.Column(name='zhalo', format='E', array=zhalo),
+        fits.Column(name='lmhalo', format='E', array=lmhalo),
+            ]
+
+    t = fits.BinTableHDU.from_columns(fits.ColDefs(tabla))
+    hdu = fits.PrimaryHDU(header=header)
+    hdul = fits.HDUList([hdu, t])
+
+    hdul.writeto('tests/testcat_normal.fits', overwrite=True)
+
+
+if __name__ == '__main__':
+    normal_cat()
