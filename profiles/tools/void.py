459931230c3a9eb700c112c5da7a59fff7b1ee24
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-25 21:27:17 -0300

    adding subclass StackedVoid for mehtod3 in radial_profiles, adding center option in get_tracers

--- profiles/tools/void.py
+++ profiles/tools/void.py
@@ -23,16 +23,27 @@ class Void:
         self.is_sorted = False
 
 
-    def get_tracers(self, cat, RMAX:float):
-        distance = np.sqrt( (cat.xhalo - self.xc)**2 +  (cat.yhalo - self.yc)**2 + (cat.zhalo - self.zc)**2 )
-        mask = distance<=(RMAX*self.rv)
+    def get_tracers(self, cat, RMAX:float, center:bool = False):
+        distance = (cat.xhalo - self.xc)**2 +  (cat.yhalo - self.yc)**2 + (cat.zhalo - self.zc)**2
+        mask = distance<=(RMAX*self.rv)**2
 
-        cat = cat[mask] # ver si esto no es el problema! (idem a perfiles3d.py de codes_tesis/)
+        cat = cat[mask]
 
-        for i in range(len(cat)):
-            t = Tracer(cat.xhalo[i], cat.yhalo[i], cat.zhalo[i], cat.lmhalo[i])
-            t.d = distance
-            self.tr.append(t)
+        if not center:
+            for i in range(len(cat)):
+                t = Tracer(cat.xhalo[i], cat.yhalo[i], cat.zhalo[i], cat.lmhalo[i])
+                t.distanceto(self.xc, self.yc, self.zc)
+                self.tr.append(t)
+        else:
+            xhalo, yhalo, zhalo = cat.xhalo - self.xc, cat.yhalo - self.yc, cat.zhalo - self.zc
+            xhalo /= self.rv 
+            yhalo /= self.rv 
+            zhalo /= self.rv 
+            for i in range(len(cat)):
+                t = Tracer(xhalo[i], yhalo[i], zhalo[i], cat.lmhalo[i])
+                t.distanceto(0,0,0)
+                self.tr.append(t)
+        
 
     def sort_tracers(self):
         self.tr.sort(key = lambda x: x.d)
@@ -52,8 +63,43 @@ class Void:
         radius = RMIN
         i = 0
 
-        self.get_tracers(cat=cat, RMAX=RMAX)
-        self.sort_tracers()
+        if not self.is_sorted or self.tr == []:
+            self.get_tracers(cat=cat, RMAX=RMAX)
+            self.sort_tracers()
+
+        for n in range(NBINS):
+            mass = 0.
+            while (self.tr[i].d >= radius and self.tr[i].d < radius + dr):
+                mass += 10.0**(self.tr[i].lm)
+                i+=1
+
+            volume = (4*np.pi/3)*((radius+dr)**3 - radius**3) # volumen del cascaron
+            Delta[n] = (mass/volume)/MeanDenTrac - 1. 
+            radius += dr
+
+        return Delta
+    
+class StackedVoid(Void):
+    def __init__(self, tracers):
+        self.tr = tracers
+        self.is_sorted = False
+
+    def radial_density_profile(self, RMIN:float, RMAX:float, dr:float):
+
+        # MeanDenTrac = 1 => Delta + 1 == Density
+        MeanDenTrac = 1. # Numero de trazadores en el catalogo / volumen total del catalogo
+        NBINS = int(round(((RMAX-RMIN)/dr),0))
+
+        # RMIN *= self.rv
+        # RMAX *= self.rv
+        # dr   *= self.rv
+
+        Delta = np.zeros(NBINS)
+        radius = RMIN
+        i = 0
+
+        if not self.is_sorted:
+            self.sort_tracers()
 
         for n in range(NBINS):
             mass = 0.
