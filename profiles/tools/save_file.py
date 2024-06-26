f5e4d04ac2cbd53de92bfaf40e6111b08f357124
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-25 21:27:43 -0300

    updating plot for test

--- profiles/tools/save_file.py
+++ profiles/tools/save_file.py
@@ -5,5 +5,9 @@ def save_file(*args):
     import matplotlib.pyplot as plt
 
     plt.figure()
-    plt.plot(args[0], c='k')
+    plt.plot(args[0], c='k', label='method1')
+    plt.plot(args[1], c='r', label='method2')
+    plt.plot(args[2], c='b', label='method3')
+
+    plt.legend()
     plt.show()
