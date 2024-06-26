e5b02f2ac8fd13d9bae1c1866cefad40281e01d5
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-26 16:43:59 -0300

    setting to save results from radial_profiles

--- profiles/tools/save_file.py
+++ profiles/tools/save_file.py
@@ -1,13 +1,29 @@
 # guardar archivos
-import astropy.io as fits
+from astropy.io import fits
+import numpy as np
 
-def save_file(*args):
-    import matplotlib.pyplot as plt
+def save_file(folder, filename,
+              stacked_profile, nvoids, RMIN, RMAX, dr,
+              Rv_min, Rv_max, z_min, z_max):
 
-    plt.figure()
-    plt.plot(args[0][0], c='k', label='method1')
-    plt.plot(args[0][1], c='r', label='method2')
-    plt.plot(args[0][2], c='b', label='method3')
+    header = fits.Header()
+    header.append(('nvoids', nvoids))
+    header.append(('rv_min', Rv_min))
+    header.append(('rv_max', Rv_max))
+    header.append(('z_min', z_min))
+    header.append(('z_max', z_max))
 
-    plt.legend()
-    plt.show()
+    NBINS = int(round((RMAX-RMIN)/dr,0))
+    bines = np.linspace(RMIN,RMAX,num=NBINS+1)
+    R = (bines[:-1] + np.diff(bines)*0.5)
+
+    table = [fits.Column(name='R', format='E', array=R),
+             fits.Column(name='stacked_profile', format='E', array=stacked_profile),
+            ]
+
+    tbhdu = fits.BinTableHDU.from_columns(fits.ColDefs(table))
+    primary_hdu = fits.PrimaryHDU(header=header)
+    hdul = fits.HDUList([primary_hdu, tbhdu])
+    hdul.writeto(folder+filename, overwrite=True)
+
+    return True
