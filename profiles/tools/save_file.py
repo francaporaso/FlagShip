9564d70cc158d809f5a0cc3b43319bba583d48c0
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-06-26 01:48:41 -0300

    test plots

--- profiles/tools/save_file.py
+++ profiles/tools/save_file.py
@@ -5,9 +5,9 @@ def save_file(*args):
     import matplotlib.pyplot as plt
 
     plt.figure()
-    plt.plot(args[0], c='k', label='method1')
-    plt.plot(args[1], c='r', label='method2')
-    plt.plot(args[2], c='b', label='method3')
+    plt.plot(args[0][0], c='k', label='method1')
+    plt.plot(args[0][1], c='r', label='method2')
+    plt.plot(args[0][2], c='b', label='method3')
 
     plt.legend()
     plt.show()
