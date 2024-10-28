1c523cc9e92cd9895ad80105dd8291871072e0a3
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-10-28 11:40:23 +0100

    debugging: debug test xddd

--- profiles/test_perfiles.py
+++ profiles/test_perfiles.py
@@ -12,28 +12,28 @@ a = {
     'filename':'test', 'lensname':'server', 'tracname':'server',
 }
 
-if (a.tracname == 'local') or (a.lensname == 'local'):
-    a.lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
-    a.tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice_halos_cut.fits"
+if (a['tracname'] == 'local') or (a['lensname'] == 'local'):
+    a['lensname'] = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
+    a['tracname'] = "/home/franco/FAMAF/Lensing/cats/MICE/mice_halos_cut.fits"
 else:
-    a.tracname = '/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits'
-    a.lensname = '/mnt/simulations/MICE/voids_MICE.dat'
+    a['tracname'] = '/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits'
+    a['lensname'] = '/mnt/simulations/MICE/voids_MICE.dat'
 
-a.filename = "radialprof_TEST.csv"
+a['filename'] = "radialprof_TEST.csv"
 
 
 def perfiles_serie():
 
-    L, _, _ = lenscat_load(a.Rv_min, a.Rv_max, a.z_min, a.z_max, a.rho1_min, a.rho1_max, a.rho2_min, a.rho2_max,
-                            flag=a.flag, lensname=a.lensname,
+    L, _, _ = lenscat_load(a['Rv_min'], a['Rv_max'], a['z_min'], a['z_max'], a['rho1_min'], a['rho1_max'], a['rho2_min'], a['rho2_max'],
+                            flag=a['flag'], lensname=a['lensname'],
                             split=False)
 
-    mass  = np.zeros(a.NBINS)
-    halos = np.zeros(a.NBINS)
+    mass  = np.zeros(a['NBINS'])
+    halos = np.zeros(a['NBINS'])
     massball  = 0.0
     halosball = 0.0
 
-    profs = [partial_profile(a.RMIN, a.RMAX, a.NBINS, L[1,0], L[5,0], L[6,0], L[7,0]) for i in range(50)]
+    profs = [partial_profile(a['NBINS'], L[1,0], L[5,0], L[6,0], L[7,0]) for i in range(50)]
 
     for res in profs:
         mass  += res[0]
@@ -41,16 +41,16 @@ def perfiles_serie():
         massball  += res[2]
         halosball += res[3]
 
-    meandenball   = (massball/(4*np.pi/3 * (5*a.RMAX)**3))
-    meanhalosball = (halosball/(4*np.pi/3 * (5*a.RMAX)**3))
+    meandenball   = (massball/(4*np.pi/3 * (5*a['RMAX'])**3))
+    meanhalosball = (halosball/(4*np.pi/3 * (5*a['RMAX'])**3))
 
-    DR = (a.RMAX-a.RMIN)/a.NBINS
+    DR = (a['RMAX']-a['RMIN'])/a['NBINS']    
     
-    vol    = np.zeros(a.NBINS)
-    volcum = np.zeros(a.NBINS)
-    for k in range(a.NBINS):
-        vol[k]    = ((k+1.0)*DR + a.RMIN)**3 - (k*DR + a.RMIN)**3
-        volcum[k] = ((k+1.0)*DR + a.RMIN)**3
+    vol    = np.zeros(a['NBINS'])
+    volcum = np.zeros(a['NBINS'])
+    for k in range(a['NBINS']):
+        vol[k]    = ((k+1.0)*DR + a['RMIN'])**3 - (k*DR + a['RMIN'])**3
+        volcum[k] = ((k+1.0)*DR + a['RMIN'])**3
     
     vol    *= (4*np.pi/3)
     volcum *= (4*np.pi/3)
@@ -60,20 +60,20 @@ def perfiles_serie():
     DeltaCum = np.cumsum(mass)/volcum/meandenball - 1
     DeltaHalosCum = np.cumsum(halos)/volcum/meanhalosball - 1
 
-    print(f"Saving in: 'serie_'{a.filename}")
+    print(f"Saving in: 'serie_'{a['filename']}")
     data = np.column_stack((Delta, DeltaCum, DeltaHalos, DeltaHalosCum))
-    np.savetxt("serie_"+a.filename, data, delimiter=',')
+    np.savetxt("serie_"+a['filename'], data, delimiter=',')
 
     return 0
 
 def perfiles_paralelo():
     
-    L, _, _ = lenscat_load(a.Rv_min, a.Rv_max, a.z_min, a.z_max, a.rho1_min, a.rho1_max, a.rho2_min, a.rho2_max,
-                                flag=a.flag, lensname=a.lensname,
-                                split=True, NSPLITS=a.NCORES)
+    L, _, _ = lenscat_load(a['Rv_min'], a['Rv_max'], a['z_min'], a['z_max'], a['rho1_min'], a['rho1_max'], a['rho2_min'], a['rho2_max'],
+                                flag=a['flag'], lensname=a['lensname'],
+                                split=True, NSPLITS=a['NCORES'])
 
-    mass  = np.zeros(a.NBINS)
-    halos = np.zeros(a.NBINS)
+    mass  = np.zeros(a['NBINS'])
+    halos = np.zeros(a['NBINS'])
     massball  = 0.0
     halosball = 0.0
 
@@ -84,16 +84,16 @@ def perfiles_paralelo():
         num = len(Li)
         if num==1:
             entrada = np.array([
-                a.RMIN, a.RMAX, a.NBINS,
+                a['NBINS'],
                 Li[1], Li[5], Li[6], Li[7],
             ])
             
             resmap = partial_profile(*entrada)
 
         else:
-            RMIN_a = np.full(num, a.RMIN)
-            RMAX_a = np.full(num, a.RMAX)
-            NBINS_a = np.full(num, a.NBINS)
+            RMIN_a = np.full(num, a['RMIN'])
+            RMAX_a = np.full(num, a['RMAX'])
+            NBINS_a = np.full(num, a['NBINS'])
             entrada = np.array([
                 RMIN_a, RMAX_a, NBINS_a, 
                 Li.T[1], Li.T[5], Li.T[6], Li.T[7],
@@ -113,33 +113,33 @@ def perfiles_paralelo():
         if i==4:
             break ## así agarro 50 voids (i=4 => 5 it del for externo => 10 voids/it* 5 it = 50 voids)
 
-    meandenball   = (massball/(4*np.pi/3 * (5*a.RMAX)**3))
-    meanhalosball = (halosball/(4*np.pi/3 * (5*a.RMAX)**3))
+    meandenball   = (massball/(4*np.pi/3 * (5*a['RMAX'])**3))
+    meanhalosball = (halosball/(4*np.pi/3 * (5*a['RMAX'])**3))
 
-    DR = (a.RMAX-a.RMIN)/a.NBINS
+    DR = (a['RMAX']-a['RMIN'])/a['NBINS']
     
-    vol    = np.zeros(a.NBINS)
-    volcum = np.zeros(a.NBINS)
-    for k in range(a.NBINS):
-        vol[k]    = ((k+1.0)*DR + a.RMIN)**3 - (k*DR + a.RMIN)**3
-        volcum[k] = ((k+1.0)*DR + a.RMIN)**3
+    vol    = np.zeros(a['NBINS'])
+    volcum = np.zeros(a['NBINS'])
+    for k in range(a['NBINS']):
+        vol[k]    = ((k+1.0)*DR + a['RMIN'])**3 - (k*DR + a['RMIN'])**3
+        volcum[k] = ((k+1.0)*DR + a['RMIN'])**3
     
     vol    *= (4*np.pi/3)
     volcum *= (4*np.pi/3)
 
-    Delta    = np.zeros(a.NBINS)
-    DeltaCum = np.zeros(a.NBINS)
-    DeltaHalos    = np.zeros(a.NBINS)
-    DeltaHalosCum = np.zeros(a.NBINS)
+    Delta    = np.zeros(a['NBINS'])
+    DeltaCum = np.zeros(a['NBINS'])
+    DeltaHalos    = np.zeros(a['NBINS'])
+    DeltaHalosCum = np.zeros(a['NBINS'])
 
     Delta = mass/vol/meandenball - 1
     DeltaHalos = halos/vol/meanhalosball - 1
     DeltaCum = np.cumsum(mass)/volcum/meandenball - 1
     DeltaHalosCum = np.cumsum(halos)/volcum/meanhalosball - 1    
 
-    print(f"Saving in: 'paralelo_'{a.filename}")
+    print(f"Saving in: 'paralelo_'{a['filename']}")
     data = np.column_stack((Delta, DeltaCum, DeltaHalos, DeltaHalosCum))
-    np.savetxt("paralelo_"+a.filename, data, delimiter=',')
+    np.savetxt("paralelo_"+a['filename'], data, delimiter=',')
 
     return 0
 
