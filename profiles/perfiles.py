262e3fdc654e46b8ad4fab4dca040980250c3f95
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-10-30 11:46:59 +0100

    update

--- profiles/perfiles.py
+++ profiles/perfiles.py
@@ -249,12 +249,9 @@ if __name__ == "__main__":
     else:
         tipo = 'A'
 
-    if (a.filename!='test'):
+    if (a.filename[:4] !='test'):
         a.filename = "radialprof_R{:.0f}_{:.0f}_z{:.1f}_{:.1f}_type{}".format(a.Rv_min, a.Rv_max, a.z_min, a.z_max, tipo)
     a.filename += '.csv'
-
-    print(a.filename)
-    assert False
 
     ## opening tracers file and general masking
     with fits.open(a.tracname) as f:
