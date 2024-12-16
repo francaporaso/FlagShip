cd8b6fcb3ca1ac1524872ff6a65323e90589f795
francaporaso <fmcaporaso@mi.unc.edu.ar>
2024-12-16 13:34:49 -0300

    "fixed" bug que no tomaba todos los cores... no es lo mas elegante igual

--- profiles/tests/voids/density_profiles_v2.py
+++ profiles/tests/voids/density_profiles_v2.py
@@ -26,11 +26,9 @@ def tracercat_load(catname='mice_sats_18939.fits', if_centrals=True):
     
     return ra_gal, dec_gal, z_gal
 
-def density_v2(N, m, xh, yh, zh, lmhalo, void_prop):
-    rv = void_prop[0]
-    xv = void_prop[1]
-    yv = void_prop[2]
-    zv = void_prop[3]
+def density_v2(xh, yh, zh, lmhalo, N, m, rv, xv, yv, zv):
+    N = int(N)
+    m = int(m)
     number_gx = np.zeros(N)
     mass_bin = np.zeros(N)
     vol = np.zeros(N)
@@ -51,10 +49,45 @@ def density_v2(N, m, xh, yh, zh, lmhalo, void_prop):
     
     return number_gx, mass_bin, vol, np.full_like(vol, mean_den_ball)
 
+cosmo = LambdaCDM(H0=100, Om0=0.25, Ode0=0.75)
+ra_gal, dec_gal, z_gal, lmhalo = tracercat_load()    
+xh, yh, zh = ang2xyz(ra_gal, dec_gal, z_gal, cosmo=cosmo)
+print('# of gx: ', len(xh))
+
+partial_func = partial(density_v2, xh, yh, zh, lmhalo)
+def partial_func_unpack(A):    
+    global xh, yh, zh, lmhalo
+    return partial_func(*A)
+
+def save_file(filename, lens_args, nvoids, N, m, P):
+
+    name_args = ('Rv_min','Rv_max','z_min','z_max','rho1_min','rho1_max','rho2_min','rho_max')
+    r_ad = np.linspace(0, m, N+1)
+    r_ad = r_ad[:-1] + np.diff(r_ad)*0.5
+    
+    h = fits.Header()
+    h.append(('Nvoids', int(nvoids)))
+    
+    for it,val in zip(name_args, lens_args):
+        h.append((it, val))
+
+    primary_hdu = fits.PrimaryHDU(header=h)
+    hdul = fits.HDUList([primary_hdu])
+
+    for p in P:
+        r_v = (p[2][0]*3/(4*np.pi))**(1/3)*(N/m)
+        table = np.array([fits.Column(name='r_phys', format='E', array=r_v*r_ad),
+                          fits.Column(name='number', format='E', array=p[0]),
+                          fits.Column(name='mass', format='E', array=p[1]),
+                          fits.Column(name='vol_phys', format='E', array=p[2]),
+                         ])
+        tbhdu = fits.BinTableHDU.from_columns(table)
+        hdul.append(tbhdu)
+        
+    hdul.writeto(filename, overwrite=True)
+    
 def main(lens_args=(6.0,9.0,0.2,0.3,-1.0,-0.8,0.0,100),
          ncores=32, N=10, m=5):
-    
-    cosmo = LambdaCDM(H0=100, Om0=0.25, Ode0=0.75)
 
     L,_,nvoids = lenscat_load(*lens_args, 
                               flag=2.0, lensname="/mnt/simulations/MICE/voids_MICE.dat",
@@ -62,27 +95,34 @@ def main(lens_args=(6.0,9.0,0.2,0.3,-1.0,-0.8,0.0,100),
     
     print('# of voids: ',nvoids)
     
-    ra_gal, dec_gal, z_gal, lmhalo = tracercat_load()    
-    xh, yh, zh = ang2xyz(ra_gal, dec_gal, z_gal, cosmo=cosmo)
-    print('# of gx: ', len(ra_gal))
-    
-    ## func for paralellization, returns func w 
-    partial_func = partial(density_v2, N, m, xh, yh, zh, lmhalo)
-    
     P = np.zeros((nvoids, 4, N)) # 4=num de arr q devuelve density_v2
     for i,Li in enumerate(tqdm(L)):
         num = len(Li)
-        print(num)
-        entrada = np.array([Li.T[1], Li.T[5], Li.T[6], Li.T[7]]).T
+        entrada = np.array([np.full(num,N),
+                            np.full(num,m),
+                            Li.T[1], #rv
+                            Li.T[5], #xv
+                            Li.T[6], #yv
+                            Li.T[7], #zv
+                           ]).T
         with Pool(processes=num) as pool:
-            resmap = pool.map(partial_func,
-                              entrada)
+            resmap = pool.map(partial_func_unpack, entrada)
             pool.close()
             pool.join()
         for j,res in enumerate(resmap):
             P[i*num + j] = res
-
-    return P
+    
+    if lens_args[7]<=0:
+        t = 'R'
+    elif lens_args[6]>=0:
+        t = 'S'
+    else:
+        t = 'all'
+    
+    filename = f'{lensname.split("/")[-1][:-4]}_Rv{int(lens_args[0])}-{int(lens_args[1])}_z0{int(10*lens_args[2])}-0{int(10*lens_args[3])}_type-{t}.fits'
+    print('Saving in '+filename)
+    savefile(filename, lens_args, nvoids, N, m, P)
+    print('End! :)')
     
 if __name__ == '__main__':
-    main()
+    main(lens_args=(6.0,9.622,0.2,0.4,-1.0,-0.8,-1.0,100.0),ncores=32,N=50,m=5)
