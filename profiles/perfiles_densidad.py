f5108fe96d1b71b9b74e74955b1cdc5ab02658d1
francaporaso <fmcaporaso@mi.unc.edu.ar>
2025-01-15 19:48:52 -0300

    script version of the notebook

--- profiles/perfiles_densidad.py
+++ profiles/perfiles_densidad.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import numpy as np
@@ -19,23 +19,21 @@ from multiprocessing import Pool
 from functools import partial
 
 
-# In[5]:
+# In[2]:
 
 
 cosmo = LambdaCDM(H0=100, Om0=0.25, Ode0=0.75)
 
 
-# In[6]:
+# In[3]:
 
 
 ## ------ PARAMS
-N = 50 ## Num de puntos del perfil
+N = 25 ## Num de puntos del perfil
 m = 5 ## dist maxima en R_v del perfil
-lensargs = (6.0,9.622,0.2,0.4,-1.0,-0.8,-1.0,100.0) ## args del cto d voids
-ncores = 64
 
 
-# In[7]:
+# In[4]:
 
 
 def lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, 
@@ -83,7 +81,7 @@ def lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho
     return L, K, nvoids
 
 
-# In[8]:
+# In[5]:
 
 
 def tracercat_load(catname='/home/fcaporaso/cats/MICE/mice_sats_18939.fits',
@@ -113,7 +111,7 @@ def tracercat_load(catname='/home/fcaporaso/cats/MICE/mice_sats_18939.fits',
             return xh, yh , zh
 
 
-# In[9]:
+# In[6]:
 
 
 def cov_matrix(array):
@@ -132,7 +130,26 @@ def cov_matrix(array):
     return COV
 
 
-# In[10]:
+# In[7]:
+
+
+def mean_density_comovingshell(xh, yh, zh, logmh,
+                               m, rv, xv, yv, zv):
+
+    dist_void = np.sqrt(xv**2 + yv**2 + zv**2)
+    dist = np.sqrt(xh**2 + yh**2 + zh**2)
+    chi_min = dist_void - m*rv
+    chi_max = dist_void + m*rv
+
+    lmh = logmh[(dist > chi_min)&(dist < chi_max)]
+
+    vol = (1/8)*(4*np.pi/3)*(chi_max**3 - chi_min**3)
+    mass = np.sum(10.0 ** lmh)
+
+    return mass/vol, len(lmh)/vol
+
+
+# In[8]:
 
 
 def number_density_v2(N, m, xh, yh, zh, lmhalo, rv, xv, yv, zv):
@@ -142,14 +159,17 @@ def number_density_v2(N, m, xh, yh, zh, lmhalo, rv, xv, yv, zv):
     dist = np.sqrt((xh-xv)**2 + (yh-yv)**2 + (zh-zv)**2) ## dist to center of void i
     const = m*rv/N
 
-    mask_mean = (dist < 5*m*rv)
+    mask_mean = (dist < 1.1*m*rv)
     logmass = lmhalo[mask_mean]
     dist = dist[mask_mean]
 
-    mass_ball = np.sum( 10.0**(logmass) )
-    vol_ball = (4/3)*np.pi*(5*m*rv)**3
-    mean_gx_ball = np.sum(mask_mean)/vol_ball
-    mean_den_ball = mass_ball/vol_ball
+    mean_den_com, mean_gx_com = mean_density_comovingshell(xh,yh,zh,lmhalo,
+                                                           m,rv,xv,yv,zv)
+    
+    # mass_ball = np.sum( 10.0**(logmass) )
+    # vol_ball = (4/3)*np.pi*(5*m*rv)**3
+    # mean_gx_ball = np.sum(mask_mean)/vol_ball
+    # mean_den_ball = mass_ball/vol_ball
     
     for k in range(N):
         mask = (dist < (k+1)*const) & (dist >= k*const)
@@ -159,10 +179,10 @@ def number_density_v2(N, m, xh, yh, zh, lmhalo, rv, xv, yv, zv):
     
     vol *= (4/3)*np.pi*const**3
     
-    return number_gx, mass_bin, vol, np.full_like(vol, mean_gx_ball), np.full_like(vol, mean_den_ball)
+    return number_gx, mass_bin, vol, np.full_like(vol, mean_gx_com), np.full_like(vol, mean_den_com)
 
 
-# In[11]:
+# In[9]:
 
 
 partial_func = partial(number_density_v2, N, m, *tracercat_load())
@@ -170,50 +190,10 @@ def partial_func_unpack(A):
     return partial_func(*A)
 
 
-# In[18]:
+# In[10]:
 
 
-def stacking(N, m, 
-             lensargs,
-             L, K, nvoids,
-             nk = 100):
-    
-    print(f"nvoids: {nvoids}")
-
-    numbergx = np.zeros((nk+1,N))
-    massbin = np.zeros((nk+1,N))
-    mu = np.zeros((nk+1,N)) ## vol * denball_5
-    mu_gx = np.zeros((nk+1,N)) ## vol * ngal_ball_5
-
-    count = 0
-    for i,Li in enumerate(tqdm(L)):
-        num=len(Li)
-        entrada = np.array([Li.T[1], Li.T[5], Li.T[6], Li.T[7]]).T
-        with Pool(processes=num) as pool:
-            resmap = pool.map(partial_func_unpack,
-                           entrada)
-            pool.close()
-            pool.join()
-        
-        for j, res in enumerate(resmap):
-            km = np.tile(K[i][j], (N,1)).T
-            numbergx += np.tile(res[0], (nk+1,1))*km
-            massbin += np.tile(res[1], (nk+1,1))*km
-            mu += np.tile(res[2]*res[4], (nk+1,1))*km
-            mu_gx += np.tile(res[2]*res[3], (nk+1,1))*km
-
-    delta = massbin/mu - 1
-    deltagx = numbergx/mu_gx - 1
-    cov_delta = cov_matrix(delta[1:,:])
-    cov_deltagx = cov_matrix(deltagx[1:,:])
-    
-    return delta[0], deltagx[0], cov_delta, cov_deltagx
-
-
-# In[13]:
-
-
-def saveresults(args,
+def saveresults(args,nvoids,sample,
                *results):
     h = fits.Header()
     
@@ -228,6 +208,9 @@ def saveresults(args,
     h.append(('rho2_max', args[7]))
     h.append(('rmax', m))
 
+    primary_hdu = fits.PrimaryHDU(header=h)
+    hdul = fits.HDUList([primary_hdu])
+    
     rrr = np.linspace(0,m,N+1)
     rrr = rrr[:-1] + np.diff(rrr)*0.5
     
@@ -236,7 +219,7 @@ def saveresults(args,
                       fits.Column(name='deltagx', format='E', array=results[1]),
                      ])
     table_cov = np.array([fits.Column(name='cov_delta', format='E', array=results[2].flatten()),
-                          fits.Column(name='cov_deltagx', format='E', array=results[3]).flatten(),
+                          fits.Column(name='cov_deltagx', format='E', array=results[3].flatten()),
                      ])
 
     hdul.append(fits.BinTableHDU.from_columns(table_delta))
@@ -249,19 +232,108 @@ def saveresults(args,
     else:
         t = 'all'
     
-    hdul.writeto(f'TEST_density_mice_Rv{int(args[0])}-{int(args[1])}_z0{int(10*args[2])}-0{int(10*args[3])}_type{t}.fits')
+    hdul.writeto(f'density_mice_mdcs_Rv{int(args[0])}-{int(args[1])}_{t}_z0{int(10*args[2])}-0{int(10*args[3])}_{sample}.fits')
 
 
-# In[ ]:
+# In[11]:
+
+
+def stacking(N, m, 
+             lensargs,
+             sample,
+             L, K, nvoids,
+             nk = 100):
+    
+    print(f"nvoids: {nvoids}")
+
+    # COVARIANZA JACKKNIFE
+    # numbergx = np.zeros((nk+1,N))
+    # massbin = np.zeros((nk+1,N))
+    # mu = np.zeros((nk+1,N)) ## vol * denball_5
+    # mu_gx = np.zeros((nk+1,N)) ## vol * ngal_ball_5
+    
+    # POISSON
+    numbergx = np.zeros((nvoids,N))
+    mu_gx = np.zeros((nvoids,N)) ## vol * meangxcomsh
+
+    massbin = np.zeros((nvoids,N))
+    mu = np.zeros((nvoids,N)) ## vol * meandencomsh
+
+    count = 0
+    for i,Li in enumerate(tqdm(L)):
+        num=len(Li)
+        entrada = np.array([Li.T[1], Li.T[5], Li.T[6], Li.T[7]]).T
+        with Pool(processes=num) as pool:
+            resmap = pool.map(partial_func_unpack,
+                           entrada)
+            pool.close()
+            pool.join()
+        
+        for j, res in enumerate(resmap):
+            #COVARIANZA JACKKNIFE
+            #km = np.tile(K[i][j], (N,1)).T
+            #numbergx += np.tile(res[0], (nk+1,1))*km
+            #massbin += np.tile(res[1], (nk+1,1))*km
+            #mu += np.tile(res[2]*res[4], (nk+1,1))*km
+            #mu_gx += np.tile(res[2]*res[3], (nk+1,1))*km
+
+            #POISSON
+            numbergx[count] = res[0]
+            massbin[count] = res[1]
+            mu[count] = res[2]*res[4]
+            mu_gx[count] = res[2]*res[3]
+            count+=1
+    
+    # COVARIANZA JACKKNIFE
+    # delta = massbin/mu - 1
+    # deltagx = numbergx/mu_gx - 1
+    # cov_delta = cov_matrix(delta[1:,:])
+    # cov_deltagx = cov_matrix(deltagx[1:,:])
+    # saveresults(lensargs, nvoids, sample, delta[0], deltagx[0], cov_delta, cov_deltagx)
+    
+    # POISSON
+    Ngx = np.sum(numbergx,axis=0)
+    Msum = np.sum(massbin,axis=0)
+    mu_sum = np.sum(mu,axis=0)
+    e_mu = np.std(mu,axis=0)
+    
+    delta = Msum/mu_sum - 1
+    deltagx = Ngx/np.sum(mu_gx,axis=0) - 1
+    e_delta = np.sqrt( Ngx + (Msum*e_mu/mu_sum)**2 )/mu_sum
+    
+    if lensargs[7]<=0:
+        t = 'R'
+    elif lensargs[6]>=0:
+        t = 'S'
+    else:
+        t = 'all'
+    
+    np.savetxt(f'density_mice_mdcs_Rv{int(lensargs[0])}-{int(lensargs[1])}_{t}_z0{int(10*lensargs[2])}-0{int(10*lensargs[3])}_{sample}.fits')
+    
+
+
+# In[16]:
 
 
 ### -------- RUN
-saveresults(
-    lensargs,
+ncores = 64
+args_list = [
+    # (6.0,9.622,0.2,0.4,-1.0,-0.8,-1.0,100.0),
+    # (6.0,9.622,0.2,0.4,-1.0,-0.8,0.0,100.0),
+    # (6.0,9.622,0.2,0.4,-1.0,-0.8,-1.0,0.0),
+    # (9.622,50.0,0.2,0.4,-1.0,-0.8,-1.0,100.0),
+    (9.622,50.0,0.2,0.4,-1.0,-0.8,0.0,100.0),
+    # (9.622,50.0,0.2,0.4,-1.0,-0.8,-1.0,0.0),
+]
+sample = 'N25'
+
+for lensargs in args_list:
+
     stacking(
         N,
         m,
         lensargs,
+        sample,
         *lenscat_load(
             *lensargs, 
             flag=2.0, 
@@ -270,5 +342,4 @@ saveresults(
             NSPLITS=ncores
         )
     )
-)
 
